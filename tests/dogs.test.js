0d5d8ba66dc53b7771ec21a87f5292013d9e99a1
Sam-H <sam@iamasamwich.com>
2021-10-02 09:47:48 +1000

    updated tests to accept body not url

--- tests/dogs.test.js
+++ tests/dogs.test.js
@@ -3,23 +3,14 @@ const addDog = require('../db/addDog');
 const removeDog = require('../db/removeDog');
 
 let req = {
-  params: {
-    url: ''
+  body: {
+    dog: ''
   }
 };
 
 describe('dog functions', () => {
   test('addDog: lets you add a dog', () => {
-    req.params.url = 'test 1';
-    return addDog(req)
-    .then(resp => {
-      expect(resp.status).toBe(201);
-      expect(resp.message).toBe('dog added');
-    });
-  });
-
-  test('addDog: it lets you add another dog', () => {
-    req.params.url = 'test 2';
+    req.body.dog = 'test 1';
     return addDog(req)
     .then(resp => {
       expect(resp.status).toBe(201);
@@ -28,11 +19,19 @@ describe('dog functions', () => {
   });
 
   test('addDog: it wont let you add the same dog twice', () => {
-    req.params.url = 'test 1';
     return addDog(req)
     .catch(resp => {
       expect(resp.status).toBe(409);
       expect(resp.message).toBe('already there');
+    });
+  });
+
+  test('addDog: it lets you add another dog', () => {
+    req.body.dog = 'test 2';
+    return addDog(req)
+    .then(resp => {
+      expect(resp.status).toBe(201);
+      expect(resp.message).toBe('dog added');
     });
   });
 
@@ -46,7 +45,7 @@ describe('dog functions', () => {
   })
 
   test('removeDog: it removes a dog', () => {
-    req.params.url = 'test 1';
+    req.body.dog = 'test 1';
     return removeDog(req)
     .then(resp => {
       expect(resp.status).toBe(201);
@@ -55,7 +54,6 @@ describe('dog functions', () => {
   });
 
   test('removeDog: it wont remove a dog which isnt there', () => {
-    req.params.url = 'test 1';
     return removeDog(req)
     .catch(resp => {
       expect(resp.status).toBe(404);
@@ -64,7 +62,7 @@ describe('dog functions', () => {
   });
 
   test('cleaning up test cases', () => {
-    req.params.url = 'test 2';
+    req.body.dog = 'test 2';
     removeDog(req);
   })
 });
