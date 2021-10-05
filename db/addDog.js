9479f810a367066e4dedf116729070d0e35a0f0e
Sam-H <sam@iamasamwich.com>
2021-10-05 11:12:17 +1000

    db/addDog refactored with function imports

--- db/addDog.js
+++ db/addDog.js
@@ -1,39 +1,15 @@
 const db = require('./');
-const getDogs = require('./getDogs');
+const {validate, checkIfAlreadyInDB, addDogToDB, getDogs} = require('./db_functions');
 
 const addDog = (req) => {
-
-  async function validate (req) {
-    if (
-      !req.body ||
-      !req.body.dog ||
-      typeof(req.body.dog) !== 'string'
-    ) throw ({status: 406, message: 'invalid'})
-    return req.body.dog;
-  };
-
-  async function checkIfThere (url) {
-    const list = await db.getData('/dogsList');
-    if (list.indexOf(url) === -1) {
-      return url;
-    } else {
-      throw({status: 409, message: 'already there'});
-    };
-  };
-
-  async function addDog(url) {
-    await db.push('/dogsList[]', url);
-    return;
-  };
-
   return validate(req)
-  .then(checkIfThere)
-  .then(addDog)
-  .then(getDogs)
-  .then(resp => ({status: 201, message: 'dog added', list: resp.list}))
-  .catch(err => {
-    throw err;
-  });
+    .then(checkIfAlreadyInDB)
+    .then(addDogToDB)
+    .then(getDogs)
+    .then(list => ({status: 201, message: 'dog added', list}))
+    .catch(err => {
+      throw err;
+    });
 };
 
 module.exports = addDog;
---
+++ db/db_functions.js
@@ -0,0 +1,49 @@
+const db = require('./index');
+
+const validate = (req) => {
+  // takes the req, checks if there is a body.dog of type string
+  // valid -> returns the dog
+  // invalid -> throws 406
+  return new Promise((resolve, reject) => {
+    if (
+      !req.body ||
+      !req.body.dog ||
+      typeof(req.body.dog) !== 'string'
+    ) {
+      reject({status: 406, message: 'invalid'});
+    } else {
+      resolve(req.body.dog);
+    };
+  });
+};
+
+const checkIfAlreadyInDB = async (dog) => {
+  //takes the dog, checks if already in DB
+  //no -> returns the dog
+  //yes -> throws 409
+  const list = await db.getData('/dogsList');
+  if (list.indexOf(dog) === -1) {
+    return dog;
+  } else {
+    throw({status: 409, message: 'already there'});
+  };
+};
+
+const addDogToDB = async (dog) => {
+  //takes a dog and adds it to the db
+  await db.push('./dogsList[]', dog);
+  return;
+};
+
+const getDogs = async () => {
+  //gets a list of dogs from the db
+  const list = await db.getData('/dogsList');
+  return list;
+};
+
+module.exports = {
+  validate,
+  checkIfAlreadyInDB,
+  addDogToDB,
+  getDogs
+}
